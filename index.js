308783669174cf17f8009b8a8f5d9c81ef79d7f7
CestLou <alisonrebeccarobb@gmail.com>
2014-12-05 11:03:28 -0800

    more work done on app

--- index.js
+++ index.js
@@ -9,27 +9,31 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:false}));
 
+
+
+
 app.get('/', function(req, res) {
 	res.render('index');
 })
 
+
 app.post('/create', function(req, res) {
 	db.ShorteningURLS.create(req.body).done(function(err, data) {
 		var hashed = hashids.encode(data.id);
-		data.updateAttributes({shortenedHash: hashed}).done(function(finalLink){
-			res.render('create',{hash: hashed})
+		data.updateAttributes( {shortenedHash: hashed}).done(function(err, finalLink) {
+			res.render('create', {hashed: finalLink.shortenedHash} )
 		})
 	})
 })
 
+
 app.get('/:shortenedHash', function(req, res) {
 	var index = req.params.shortenedHash
-	//[where] in the command below needs to be a string.
-	db.ShorteningURLs.find({ where: {shortenedHash: index} }).then(function(URL){
-	  ShorteningURLs.shortenedHash= URL
-	  task.save().success(function() {});
-	  res.render('results');
+	db.ShorteningURLS.find( { where: {shortenedHash: index} } ).then(function(URL) {
+	  res.redirect(URL.URL);
 	})
 })
+
+
 
 app.listen(3000);
